2129138ac1bf6f7b60443fad0f9ac859ae172769
Krutik Raut <58563133+reboot13-git@users.noreply.github.com>
2021-03-16 17:21:00 +0530

    Add files via upload

---
+++ mathGameV1.py
@@ -0,0 +1,535 @@
+import time
+import random
+
+print("You Have 10 Points")
+time.sleep(1.0)
+print(".................\n")
+time.sleep(1.0)
+print("Loading>>>>>>>>>>>>\n")
+time.sleep(1.0)
+print("Generating Questions\n")
+time.sleep(1.0)
+print ("Start")
+time.sleep(2.0)
+points = 10
+
+#1
+quest = ("{} + {}".format(random.randint(10,99),random.randint(1,50) ))
+print(quest)
+solution = int(input())
+
+if solution == eval(quest):
+    points +=5
+    print("Correct")
+    print("Points: {}\n".format(points))
+else:      
+    points -=5
+    print("Incorrect")
+    print("Points: {}\n".format(points))
+if points <= 0:
+    print("Game Over")
+    exit()
+else:    
+
+#2
+ quest = ("{} - {}".format(random.randint(10,99),random.randint(1,50) ))
+print(quest)
+solution = int(input())
+
+if solution == eval(quest):
+    points +=5
+    print("Correct")
+    print("Points: {}\n".format(points))
+else:    
+    points -=5
+    print("Incorrect")
+    print("Points: {}\n".format(points))
+if points <= 0:
+    print("Game Over")
+    exit()
+else:    
+#3    
+ quest = ("{} * {}".format(random.randint(1,25),random.randint(1,10) ))
+print(quest)
+solution = int(input())
+
+if solution == eval(quest):
+    points +=5
+    print("Correct")
+    print("Points: {}\n".format(points))
+else:    
+    points -=5
+    print("Incorrect")
+    print("Points: {}\n".format(points))    
+if points <= 0:
+    print("Game Over")
+    exit()
+else:        
+
+#4    
+ quest = ("{} + {}".format(random.randint(10,99),random.randint(1,50) ))
+print(quest)
+solution = int(input())
+
+if solution == eval(quest):
+    points +=5
+    print("Correct")
+    print("Points: {}\n".format(points))
+else:    
+    points -=5
+    print("Incorrect")
+    print("Points: {}\n".format(points))    
+if points <= 0:
+    print("Game Over")
+    exit()
+else: 
+
+#5
+ quest = ("{} - {}".format(random.randint(10,99),random.randint(1,50) ))
+print(quest)
+solution = int(input())
+
+if solution == eval(quest):
+    points +=5
+    print("Correct")
+    print("Points: {}\n".format(points))
+else:    
+    points -=5
+    print("Incorrect")
+    print("Points: {}\n".format(points))    
+if points <= 0:
+    print("Game Over")
+    exit()
+else:    
+#6    
+ quest = ("{} * {}".format(random.randint(10,25),random.randint(1,20) ))
+print(quest)
+solution = int(input())
+
+if solution == eval(quest):
+    points +=5
+    print("Correct")
+    print("Points: {}\n".format(points))
+else:    
+    points -=5
+    print("Incorrect")
+    print("Points: {}\n".format(points))    
+if points <= 0:
+    print("Game Over")
+    exit()
+else: 
+#7   
+ quest = ("{} - {}".format(random.randint(10,200),random.randint(1,50) ))
+print(quest)
+solution = int(input())
+
+if solution == eval(quest):
+    points +=5
+    print("Correct")
+    print("Points: {}\n".format(points))
+else:    
+    points -=5
+    print("Incorrect")
+    print("Points: {}\n".format(points))    
+if points <= 0:
+    print("Game Over")
+    exit()
+else: 
+#8   
+ quest = ("{} * {}".format(random.randint(10,25),random.randint(1,20) ))
+print(quest)
+solution = int(input())
+
+if solution == eval(quest):
+    points +=5
+    print("Correct")
+    print("Points: {}\n".format(points))
+else:    
+    points -=5
+    print("Incorrect")
+    print("Points: {}\n".format(points))    
+if points <= 0:
+    print("Game Over")
+    exit()
+else:         
+#9  
+ quest = ("{} + {}".format(random.randint(10,99),random.randint(1,50) ))
+print(quest)
+solution = int(input())
+
+if solution == eval(quest):
+    points +=5
+    print("Correct")
+    print("Points: {}\n".format(points))
+else:    
+    points -=5
+    print("Incorrect")
+    print("Points: {}\n".format(points))    
+if points <= 0:
+    print("Game Over")
+    exit()
+else:     
+#10    
+ quest = ("{} - {}".format(random.randint(10,150),random.randint(1,79) ))
+print(quest)
+solution = int(input())
+
+if solution == eval(quest):
+    points +=5
+    print("Correct")
+    print("Points: {}\n".format(points))
+else:    
+    points -=5
+    print("Incorrect")
+    print("Points: {}\n".format(points))    
+if points <= 0:
+    print("Game Over")
+    exit()
+else:     
+#11  
+ quest = ("{} + {}".format(random.randint(10,99),random.randint(1,50) ))
+print(quest)
+solution = int(input())
+
+if solution == eval(quest):
+    points +=5
+    print("Correct")
+    print("Points: {}\n".format(points))
+else:    
+    points -=5
+    print("Incorrect")
+    print("Points: {}\n".format(points))    
+if points <= 0:
+    print("Game Over")
+    exit()
+else:
+#12  
+ quest = ("{} * {}".format(random.randint(10,25),random.randint(1,20) ))
+print(quest)
+solution = int(input())
+
+if solution == eval(quest):
+    points +=5
+    print("Correct")
+    print("Points: {}\n".format(points))
+else:    
+    points -=5
+    print("Incorrect")
+    print("Points: {}\n".format(points))    
+if points <= 0:
+    print("Game Over")
+    exit()
+else:
+    #13  
+ quest = ("{} + {}".format(random.randint(10,99),random.randint(1,50) ))
+print(quest)
+solution = int(input())
+
+if solution == eval(quest):
+    points +=5
+    print("Correct")
+    print("Points: {}\n".format(points))
+else:    
+    points -=5
+    print("Incorrect")
+    print("Points: {}\n".format(points))    
+if points <= 0:
+    print("Game Over")
+    exit()
+else:
+    #14  
+ quest = ("{} * {}".format(random.randint(10,25),random.randint(1,20) ))
+print(quest)
+solution = int(input())
+
+if solution == eval(quest):
+    points +=5
+    print("Correct")
+    print("Points: {}\n".format(points))
+else:    
+    points -=5
+    print("Incorrect")
+    print("Points: {}\n".format(points))    
+if points <= 0:
+    print("Game Over")
+    exit()
+else:
+    #15 
+ quest = ("{} - {}".format(random.randint(10,99),random.randint(1,50) ))
+print(quest)
+solution = int(input())
+
+if solution == eval(quest):
+    points +=5
+    print("Correct")
+    print("Points: {}\n".format(points))
+else:    
+    points -=5
+    print("Incorrect")
+    print("Points: {}\n".format(points))    
+if points <= 0:
+    print("Game Over")
+    exit()
+else:
+    #16  
+ quest = ("{} - {}".format(random.randint(10,99),random.randint(1,50) ))
+print(quest)
+solution = int(input())
+
+if solution == eval(quest):
+    points +=5
+    print("Correct")
+    print("Points: {}\n".format(points))
+else:    
+    points -=5
+    print("Incorrect")
+    print("Points: {}\n".format(points))    
+if points <= 0:
+    print("Game Over")
+    exit()
+else:
+    #17  
+ quest = ("{} * {}".format(random.randint(10,25),random.randint(1,20) ))
+print(quest)
+solution = int(input())
+
+if solution == eval(quest):
+    points +=5
+    print("Correct")
+    print("Points: {}\n".format(points))
+else:    
+    points -=5
+    print("Incorrect")
+    print("Points: {}\n".format(points))    
+if points <= 0:
+    print("Game Over")
+    exit()
+else:
+    #18 
+ quest = ("{} + {}".format(random.randint(10,99),random.randint(1,50) ))
+print(quest)
+solution = int(input())
+
+if solution == eval(quest):
+    points +=5
+    print("Correct")
+    print("Points: {}\n".format(points))
+else:    
+    points -=5
+    print("Incorrect")
+    print("Points: {}\n".format(points))    
+if points <= 0:
+    print("Game Over")
+    exit()
+else:
+    #19  
+ quest = ("{} - {}".format(random.randint(10,99),random.randint(1,50) ))
+print(quest)
+solution = int(input())
+
+if solution == eval(quest):
+    points +=5
+    print("Correct")
+    print("Points: {}\n".format(points))
+else:    
+    points -=5
+    print("Incorrect")
+    print("Points: {}\n".format(points))    
+if points <= 0:
+    print("Game Over")
+    exit()
+else:
+    #20  
+ quest = ("{} * {}".format(random.randint(10,25),random.randint(1,20) ))
+print(quest)
+solution = int(input())
+
+if solution == eval(quest):
+    points +=5
+    print("Correct")
+    print("Points: {}\n".format(points))
+else:    
+    points -=5
+    print("Incorrect")
+    print("Points: {}\n".format(points))    
+if points <= 0:
+    print("Game Over")
+    exit()
+else:
+    #21 
+ quest = ("{} + {}".format(random.randint(10,99),random.randint(1,50) ))
+print(quest)
+solution = int(input())
+
+if solution == eval(quest):
+    points +=5
+    print("Correct")
+    print("Points: {}\n".format(points))
+else:    
+    points -=5
+    print("Incorrect")
+    print("Points: {}\n".format(points))    
+if points <= 0:
+    print("Game Over")
+    exit()
+else:
+    #22  
+ quest = ("{} + {}".format(random.randint(10,99),random.randint(1,50) ))
+print(quest)
+solution = int(input())
+
+if solution == eval(quest):
+    points +=5
+    print("Correct")
+    print("Points: {}\n".format(points))
+else:    
+    points -=5
+    print("Incorrect")
+    print("Points: {}\n".format(points))    
+if points <= 0:
+    print("Game Over")
+    exit()
+else:
+    #23  
+ quest = ("{} * {}".format(random.randint(10,25),random.randint(1,20) ))
+print(quest)
+solution = int(input())
+
+if solution == eval(quest):
+    points +=5
+    print("Correct")
+    print("Points: {}\n".format(points))
+else:    
+    points -=5
+    print("Incorrect")
+    print("Points: {}\n".format(points))    
+if points <= 0:
+    print("Game Over")
+    exit()
+else:
+    #24
+ quest = ("{} * {}".format(random.randint(10,25),random.randint(1,20) ))
+print(quest)
+solution = int(input())
+
+if solution == eval(quest):
+    points +=5
+    print("Correct")
+    print("Points: {}\n".format(points))
+else:    
+    points -=5
+    print("Incorrect")
+    print("Points: {}\n".format(points))    
+if points <= 0:
+    print("Game Over")
+    exit()
+else:
+    #25  
+ quest = ("{} - {}".format(random.randint(10,99),random.randint(1,50) ))
+print(quest)
+solution = int(input())
+
+if solution == eval(quest):
+    points +=5
+    print("Correct")
+    print("Points: {}\n".format(points))
+else:    
+    points -=5
+    print("Incorrect")
+    print("Points: {}\n".format(points))    
+if points <= 0:
+    print("Game Over")
+    exit()
+else:
+    #26  
+ quest = ("{} * {}".format(random.randint(10,25),random.randint(1,20) ))
+print(quest)
+solution = int(input())
+
+if solution == eval(quest):
+    points +=5
+    print("Correct")
+    print("Points: {}\n".format(points))
+else:    
+    points -=5
+    print("Incorrect")
+    print("Points: {}\n".format(points))    
+if points <= 0:
+    print("Game Over")
+    exit()
+else:
+    #27  
+ quest = ("{} - {}".format(random.randint(10,99),random.randint(1,50) ))
+print(quest)
+solution = int(input())
+
+if solution == eval(quest):
+    points +=5
+    print("Correct")
+    print("Points: {}\n".format(points))
+else:    
+    points -=5
+    print("Incorrect")
+    print("Points: {}\n".format(points))    
+if points <= 0:
+    print("Game Over")
+    exit()
+else:
+    #28  
+ quest = ("{} * {}".format(random.randint(1,35),random.randint(1,20) ))
+print(quest)
+solution = int(input())
+
+if solution == eval(quest):
+    points +=5
+    print("Correct")
+    print("Points: {}\n".format(points))
+else:    
+    points -=5
+    print("Incorrect")
+    print("Points: {}\n".format(points))    
+if points <= 0:
+    print("Game Over")
+    exit()
+else:
+    #29  
+ quest = ("{} + {}".format(random.randint(10,99),random.randint(1,50) ))
+print(quest)
+solution = int(input())
+
+if solution == eval(quest):
+    points +=5
+    print("Correct")
+    print("Points: {}\n".format(points))
+else:    
+    points -=5
+    print("Incorrect")
+    print("Points: {}\n".format(points))    
+if points <= 0:
+    print("Game Over")
+    exit()
+else:
+    #30 
+ quest = ("{} + {}".format(random.randint(10,99),random.randint(1,50) ))
+print(quest)
+solution = int(input())
+
+if solution == eval(quest):
+    points +=5
+    print("Correct")
+    print("Points: {}\n".format(points))
+else:    
+    points -=5
+    print("Incorrect")
+    print("Points: {}\n".format(points))    
+### Game Completed
+print("Hurray game completed")
+if points == 150:
+    print("You did a great job")
+elif points > 100  and points <150:    
+    print("Good work")
+elif points < 100 and points > 75:    
+    print("Not satisfactory")
+else:   
+    print("You loose")
+ 
